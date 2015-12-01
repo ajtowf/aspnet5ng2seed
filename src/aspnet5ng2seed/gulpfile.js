54d47ec87fed5ebf09252f1e70eddb4d136cab95
ajtowf <ajden.towfeek@gmail.com>
2015-12-02 00:46:31 +0100

    aspnet5ng2 seed

---
+++ src/aspnet5ng2seed/gulpfile.js
@@ -0,0 +1,23 @@
+﻿/// <binding AfterBuild='libs' Clean='clean' />
+
+var gulp = require('gulp');
+var rimraf = require('rimraf');
+
+var paths = {
+    npm: './node_modules/',
+    lib: './wwwroot/lib/'
+};
+
+var libs = [
+    paths.npm + 'angular2/bundles/angular2.dev.js',
+    paths.npm + 'es6-shim/es6-shim.js',
+    paths.npm + 'systemjs/dist/system.js'
+];
+
+gulp.task('libs', function () {
+    return gulp.src(libs).pipe(gulp.dest(paths.lib));
+});
+
+gulp.task('clean', function (callback) {
+    rimraf(paths.lib, callback);
+});
---
+++ src/aspnet5ng2seed/scripts/app.ts
@@ -0,0 +1,16 @@
+﻿import { Component, View, bootstrap } from 'angular2/angular2';
+
+@Component({
+    selector: "my-app" 
+})
+@View({
+    template: `
+{{message}}
+<br/><input [(ng-model)]="message"/>
+`
+})
+class AppComponent {
+    message: string = "Hello world!";
+}
+
+bootstrap(AppComponent);
